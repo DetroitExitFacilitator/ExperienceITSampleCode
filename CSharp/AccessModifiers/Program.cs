1356b5d860992836ca25650d34599b6db018ca79
scottyscooter <reptilenut_77@yahoo.com>
2017-07-18 14:57:47 -0400

    Add files via upload

---
+++ CSharp/AccessModifiers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Try to instantiate a Human object here and notice what properties you are allowed to set and which ones you are not.
+        }
+    }
+}
