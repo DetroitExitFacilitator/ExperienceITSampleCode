375827f5ca191d1f60e6c8434e9d3489193c1b4d
Maciek Konkolowicz <maciekkonkolowicz@titlesource.com>
2017-07-19 10:50:19 -0400

    Added accessmodifier examples and answers

---
+++ CSharp/DataTypesAndVariables.Answers/AccessModifiersAnswers.cs
@@ -0,0 +1,44 @@
+﻿namespace DataTypesAndVariables.Answers
+{
+    public class AccessModifiersAnswers
+    {
+        public static void Main(string[] args)
+        {
+            //var person = new Human()
+            //{
+            //    Name = "Craig",
+            //    Age = 300,
+            //    BankAccountNumber = 45887,
+            //    SocialSecurityNumber = "854 - 57 - 4785"
+            //};
+
+            //var t = new Student("sam", 27, 3243, "43");
+        }
+    }
+    public class Student : Human
+    {
+        public int StudentID { get; set; }
+
+        //public Student(string name, int age, int accountNumber, string socialSecurity)
+        //{
+        //    Name = name;
+        //    Age = age;
+        //    BankAccountNumber = accountNumber;
+        //    SocialSecurityNumber = socialSecurity;
+
+        //}
+    }
+
+    public class Human
+    {
+        public string Name { get; set; }
+
+        private int Age { get; set; }
+
+        protected int BankAccountNumber { get; set; }
+
+        internal string SocialSecurityNumber { get; set; }
+    }
+}
+    }
+}
