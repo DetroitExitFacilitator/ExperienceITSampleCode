3ceb98c88f0917c1b1d12cdd8ffcfc4bc30b420f
aydinakcasu <aakcasu@gmail.com>
2017-08-09 14:06:29 -0400

    Add steps to the models.

--- SampleProject/SampleSolution/WebApiProject/Models/Image.cs
+++ SampleProject/SampleSolution/WebApiProject/Models/Image.cs
@@ -5,6 +5,9 @@ using System.Web;
 
 namespace WebApiProject.Models
 {
+    /* Step: This object is used to define an Image object*/
+    // Step 6.531: Create a public class called Image.
+    // Step 6.532: add public properties called full, square
     public class Image
     {
         public string full { get; set; }
--- SampleProject/SampleSolution/WebApiProject/Models/Links.cs
+++ SampleProject/SampleSolution/WebApiProject/Models/Links.cs
@@ -5,6 +5,9 @@ using System.Web;
 
 namespace WebApiProject.Models
 {
+    /* Step: This object is used to define a Links object*/
+    // Step 6.541: Create a public class called Links.
+    // Step 6.542: add public properties called item, resource
     public class Links
     {
         public string item { get; set; }
