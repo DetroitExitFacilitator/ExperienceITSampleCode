7e729fb18c8d43890eda91b926a8b5cb287a464e
aydinakcasu <aakcasu@gmail.com>
2017-09-29 11:09:49 -0400

    Formatting, add note.
    Need more explaination.

--- CSharp/DataTypesAndVariables/Exercise1/Operators.cs
+++ CSharp/DataTypesAndVariables/Exercise1/Operators.cs
@@ -1,5 +1,5 @@
 using System.Collections.Generic;
-using System;﻿
+using System;
 namespace DataTypesAndVariables.Exercise1
 {
     public class Operators
@@ -7,27 +7,28 @@ namespace DataTypesAndVariables.Exercise1
         public static void Main(string[] args)
         {
             /*
-            += -= *= /= %= are different types of mathematical operators.So, addition(+), subtraction(-), multiplication(*), division(/) and the Modulo operator(%).
+            += -= *= /= %= are different types of mathematical operators. So, addition(+), subtraction(-), multiplication(*), division(/) and the Modulo operator(%).
+            
+            Note: These operators are just used as a shorthand.  For example, value = value + 5, can be written as value += 5.
 
             Now, the "=" also plays a part in these operations. For example, consider the addition operator(+=).The '+' will actually perform the addtion while the '=' will assign that value to the variable.
 
             So, if we declare that X = 5 then 'X += 2' will set the value of X as 7.The '+' adds 2 to 5 and the '=' assigns the value 7 to the variable X.
-
-
             */
 
             //Write a program that declares a variable with an initial value of 9.
 
-            //Now use the addition operator to add the variable with itself
+            //Have that integer use the addition operator to add itself with itself
 
-            //use that same variable again and subtract 2 from itself using the subtraction operator
+            //Use that integer again and subtract 2 from itself using the subtraction operator
 
-            //use the multiplication operator to multiply the integer by 8
+            //Use the multiplication operator to multiply the integer by 8
 
-            //use the division operator to divide the integer by 3
+            //Use the division operator to divide the integer by 3
 
-            //use the modulus operator to mod the integer by 10
+            //Use the modulus operator to mod the integer by 10
 
+            Console.ReadKey();
         }
     }
 }
