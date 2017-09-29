90bd6b567995f00373d0c075dd26a82a4aa3bdf9
aydinakcasu <aakcasu@gmail.com>
2017-09-29 12:25:03 -0400

    Update Collections.

--- CSharp/DataTypesAndVariables.Answers/Collections.cs
+++ CSharp/DataTypesAndVariables.Answers/Collections.cs
@@ -7,8 +7,8 @@ namespace DataTypesAndVariables.Answers
     {
         public static void Main(string[] args)
         {
-            //array
-            //write a program that creates an array of integers called "odds" with a size of 5 elements
+            //Array
+            //Write a program that creates an array of integers called "odds" with a size of 5 elements
             int[] odds = new int[5];
             //Then create a loop that will add the odd numbers between 0 and 10 to the array
             int nextOddSlot = 0;
@@ -19,12 +19,12 @@ namespace DataTypesAndVariables.Answers
                     odds[nextOddSlot] = i;
                     nextOddSlot += 1;
                 }
-                    
+
             }
 
-            //again create a program that creates an integer array called "evens" with a size of 5 elements
+            //Again create a program that creates an integer array called "evens" with a size of 5 elements
             int[] evens = new int[5];
-            //this time create a loop that will add the even numbers between 0 and 20 to the array
+            //This time create a loop that will add the even numbers between 0 and 20 to the array
             int nextEvenSlot = 0;
             for (int i = 0; i < 20; i++)
             {
@@ -37,7 +37,7 @@ namespace DataTypesAndVariables.Answers
 
             //Finally, for arrays, create a program that will sort this array of numbers from highest to lowest
             int[] numbers = new int[] { 1, 3, 2, 4, 9, 20, 83, 88, 10, -2, -3 };
-            
+
             for (int i = 0; i < numbers.Length; i++)
             {
                 int largestNumberPosition = i;
@@ -59,13 +59,11 @@ namespace DataTypesAndVariables.Answers
             {
                 Console.Write($"{number} ");
             }
-            //Todo Look up different sorting methods online like Bubble Sort
 
+            //List
 
-            //list
-
-            //write a program that uses a list called names with the names Craig and Greg already inside the list
-            List<string> names = new List<string> {"Craig", "Greg"};
+            //Write a program that uses a list called names with the names Craig and Greg already inside the list
+            List<string> names = new List<string> { "Craig", "Greg" };
             //Then add to the list two more names
             names.Add("Luke");
             names.Add("Ren");
@@ -75,7 +73,7 @@ namespace DataTypesAndVariables.Answers
             //Remove the name "Craig" from the list
             names.Remove("Craig");
 
-            //queue
+            //Queue
             //Write a program that uses a queue to print a list of names in a FIFO(First in, First Out) order.
             Queue<string> collectionOfNames = new Queue<string>();
 
@@ -88,7 +86,7 @@ namespace DataTypesAndVariables.Answers
             collectionOfNames.Enqueue("Greg");
             collectionOfNames.Enqueue("Craig");
 
-            //print each name to the console in a first in, first out order.
+            //Print each name to the console in a first in, first out order.
             foreach (var name in collectionOfNames)
             {
                 index += 1;
@@ -108,16 +106,16 @@ namespace DataTypesAndVariables.Answers
             Console.WriteLine(collectionOfNames.Peek());
 
 
-            //dictionary
+            //Dictionary
 
-            // Write a program that uses dictionary called employeeCoolnessFactor that holds an employee's name as a string and a coolness value as an int, with the employee names Craig and Greg already inside the dictionary.
+            //Write a program that uses dictionary called employeeCoolnessFactor that holds an employee's name as a string and a coolness value as an int, with the employee names Craig and Greg already inside the dictionary.
             Dictionary<string, int> employeeCoolnessFactor = new Dictionary<string, int> { { "Craig", 10 }, { "Greg", 8 } };
-            // Then add two more names to the list
+            //Then add two more names to the list
             employeeCoolnessFactor.Add("Seth", 2);
             employeeCoolnessFactor.Add("Sam", 7);
-            // Find out how cool Craig is by retrieveing the value by using his name (the key).
+            //Find out how cool Craig is by retrieveing the value by using his name (the key).
             Console.WriteLine($"Craig is a {employeeCoolnessFactor["Craig"]}/10 on the coolness scale!");
-            // Lets compare Seth and Craig and see who is cooler.
+            //Lets compare Seth and Craig and see who is cooler.
             if (employeeCoolnessFactor["Craig"] > employeeCoolnessFactor["Seth"])
             {
                 Console.WriteLine("Craig is the COOLEST!!!");
@@ -128,29 +126,29 @@ namespace DataTypesAndVariables.Answers
             }
 
 
-            //stack
+            //Stack
 
-            // Write a program that holds 5 names in a stack.
+            //Write a program that holds 5 names in a stack.
             Stack<string> nameStack = new Stack<string>();
             nameStack.Push("Craig");
             nameStack.Push("Micky");
             nameStack.Push("Sam");
             nameStack.Push("Greg");
             nameStack.Push("Seth");
-            // Display the stack to compare
+            //Display the stack to compare
             foreach (var name in nameStack)
             {
                 Console.WriteLine($"{name}\n");
             }
-            // Use the Peek funtion to see the last item in the stack.
+            //Use the Peek funtion to see the last item in the stack.
             Console.WriteLine($"{nameStack.Peek()} is the last name added to the stack.");
-            // Use the Pop function to remove the last item from the stack.
+            //Use the Pop function to remove the last item from the stack.
             nameStack.Pop();
             foreach (var name in nameStack)
             {
                 Console.WriteLine($"{name}\n");
             }
-
+            Console.ReadKey();
         }
     }
 }
--- CSharp/DataTypesAndVariables/Exercise1/Collections.cs
+++ CSharp/DataTypesAndVariables/Exercise1/Collections.cs
@@ -7,63 +7,60 @@ namespace DataTypesAndVariables.Exercise1
     {
         public static void Main(string[] args)
         {
-            //array
-            //write a program that creates an array with a size of 5 elements
+            //Array
+            //Write a program that creates an array of integers called "odds" with a size of 5 elements
             //Then create a loop that will add the odd numbers between 0 and 10 to the array
 
-            //again create a program that creates an array with a size of 5 elements
-            //this time create a loop that will add the even numbers between 0 and 20 to the array
+            //Again create a program that creates an integer array called "evens" with a size of 5 elements
+            //This time create a loop that will add the even numbers between 0 and 20 to the array
 
             //Finally, for arrays, create a program that will sort this array of numbers from highest to lowest
-            //Todo Look up different sorting methods online like Bubble Sort
 
+            //List
 
-            //list
-
-            //write a program that uses a list with the names Craig and Greg already inside the list
+            //Write a program that uses a list called names with the names Craig and Greg already inside the list
             //Then add to the list two more names
             //Finally, add this list of names to your current list 
             List<string> myNames = new List<string> { "Sam", "Donn", "Micky", "Ramez" };
             //Remove the name "Craig" from the list
 
 
-            //queue
+            //Queue
             //A queue is a collection of objects that are stored and processed in a FIFO(First in, First out) order.
             //The object that is added in first will also be the first object to be processed.
             //Whenever an operation is applied to a queue, the objects that were added first will be processed first.
 
-
             //Write a program that uses a queue to print a list of names in a FIFO order.
 
             //Add a list of names to the queue, as many as you'd like. (at least 5)
-            //print each name to the console in a first in, first out order.
+            //Print each name to the console in a first in, first out order.
 
             //Remove the first element of the queue using dequeue() and print the names once more
 
             //Use Peek() to view the first element in the queue without removing it.
 
-            //dictionary
+            //Dictionary
 
-            // Write a program that uses dictionary called employeeCoolnessFactor that holds an 
-            //employee's name as a string and a coolness value as an int, with the employee names 
+            // Write a program that uses dictionary called employeeCoolnessFactor that holds an employee's name as a string and a coolness value as an int, with the employee names 
             //Craig and Greg already inside the dictionary.
 
-            // Then add two more names to the list
+            //Then add two more names to the list
 
-            // Find out how cool Craig is by retrieveing the value by using his name (the key).
+            //Find out how cool Craig is by retrieveing the value by using his name (the key).
 
-            // Lets compare Seth and Craig and see who is cooler.
+            //Lets compare Seth and Craig and see who is cooler.
 
-            //stack
+            //Stack
 
-            // This is a great write up of what a stack is: 'https://www.codeproject.com/Articles/17029/The-use-of-Stacks-in-C'
-            // Write a program that holds 5 names in a stack.
+            //This is a great write up of what a stack is: 'https://www.codeproject.com/Articles/17029/The-use-of-Stacks-in-C'
+            //Write a program that holds 5 names in a stack.
 
-            // Display the stack to compare
+            //Display the stack to compare
 
-            // Use the Peek funtion to see the last item in the stack.
+            //Use the Peek funtion to see the last item in the stack.
 
-            // Use the Pop function to remove the last item from the stack.
+            //Use the Pop function to remove the last item from the stack.
+            Console.ReadKey();
         }
     }
 }
