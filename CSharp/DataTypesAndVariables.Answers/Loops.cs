f6349f53466b25f580ba993fff721772fcff7c74
aydinakcasu <aakcasu@gmail.com>
2017-09-29 11:22:16 -0400

    Clean up.

--- CSharp/DataTypesAndVariables.Answers/Loops.cs
+++ CSharp/DataTypesAndVariables.Answers/Loops.cs
@@ -7,7 +7,7 @@ namespace DataTypesAndVariables.Answers
     {
         public static void Main(string[] args)
         {
-            // Do While Loop
+            //Do While Loop
 
             //Write a program that uses a do while statement. Inside the loop, a kid is asking "are we there yet"
             //Then ask the user for input. The loop ends when we have arrived and then print to the console that "We're here!"
@@ -22,10 +22,10 @@ namespace DataTypesAndVariables.Answers
 
             Console.WriteLine("We're here!\n");
 
-            //while loops
+            //While loops
 
             //Write a program using a while loop that counts by 5's up to 100 starting at 5
-            //Ex 5 10 15 20 25 30 etc
+            //Expected output: 5 10 15 20 25 30 etc
             int num = 5;
 
             Console.WriteLine("Counting by 5's up to 100");
@@ -39,8 +39,8 @@ namespace DataTypesAndVariables.Answers
 
             //For Loops
 
-            // Write a program that uses a for loop to count down from 30 to 0, by 3's
-            // Ex 30 27 24 21 18 etc
+            //Write a program that uses a for loop to count down from 30 to 0, by 3's
+            //Expected output:  30 27 24 21 18 etc
 
             Console.WriteLine("Counting down from 30 by 3's:");
             for (int i = 30; i >= 0; i -= 3)
@@ -49,7 +49,7 @@ namespace DataTypesAndVariables.Answers
             }
             Console.WriteLine();
 
-            // Post and pre increment
+            //Post and pre increment
 
             // Write a program that uses a for loop with a pre incrementing counter and stops at <= 10 and increases by 1 each time
             Console.WriteLine("Counting from 0 to 10 with post increment evaluation");
@@ -57,7 +57,7 @@ namespace DataTypesAndVariables.Answers
             {
                 Console.Write($"{i} ");
             }
-            // What is the final number printed?
+            //What is the final number printed?
 
             Console.WriteLine();
 
@@ -68,14 +68,14 @@ namespace DataTypesAndVariables.Answers
                 Console.Write($"{i} ");
             }
             Console.WriteLine();
-            // What is the final number printed?
+            //What is the final number printed?
 
 
-            // Foreach Loop
+            //Foreach Loop
 
             Console.WriteLine("Printing the even numbers with a for each loop");
             List<int> numbers = new List<int> { 1, 3, 2, 4, 9, 20, 83, 88, 10, -2, -3 };
-            // Use a foreach loop to go over this list of numbers and only print the even numbers, not the odd
+            //Use a foreach loop to go over this list of numbers and only print the even numbers, not the odd
             foreach (var number in numbers)
             {
                 if (number % 2 == 0)
--- CSharp/DataTypesAndVariables/Exercise1/Loops.cs
+++ CSharp/DataTypesAndVariables/Exercise1/Loops.cs
@@ -7,34 +7,37 @@ namespace DataTypesAndVariables.Exercise1
     {
         public static void Main(string[] args)
         {
-            //do while loop
+            //Do While Loop
 
             //Write a program that uses a do while statement. Inside the loop, a kid is asking "are we there yet"
             //Then ask the user for input. The loop ends when we have arrived and then print to the console that "We're here!"
 
-            //while loops
+            //While loops
 
             //Write a program using a while loop that counts by 5's up to 100 starting at 5
-            //Ex 5 10 15 20 25 30 etc
-
-            //for loops
+            //Expected output: 5 10 15 20 25 30 etc
 
             //Write a program that uses a for loop to count down from 30 to 0, by 3's
-            //Ex 30 27 24 21 18 etc
+            //Expected output:  30 27 24 21 18 etc
 
-            //post and pre increment
+            //For Loops
+
+            //Write a program that uses a for loop to count down from 30 to 0, by 3's
+            //Expected output:  30 27 24 21 18 etc
+
+            //Post and pre increment
 
             //write a program that uses a for loop with a pre incrementing counter and stops at <= 10 and increases by 1 each time
-            //what is the final number printed?
+            //What is the final number printed?
 
             //write a program that uses a for loop with a post incrementing counter and stops at <= 10 and increases by 1 each time
-            //what is the final number printed?
+            //What is the final number printed?
 
-            //foreach
+            //Foreach Loop
 
             List<int> numbers = new List<int> { 1, 3, 2, 4, 9, 20, 83, 88, 10, -2, -3 };
-            //use a foreach loop to go over this list of numbers and only print the even numbers, not the odd
-
+            //Use a foreach loop to go over this list of numbers and only print the even numbers, not the odd
+            Console.ReadKey();
         }
     }
 }
