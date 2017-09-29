8f35d6bb0639c45a6ea77502adc835d1455df234
aydinakcasu <aakcasu@gmail.com>
2017-09-29 10:21:03 -0400

    Update comments from answers, remove old ones.

--- CSharp/DataTypesAndVariables/Exercise1/NumbersProblem.cs
+++ CSharp/DataTypesAndVariables/Exercise1/NumbersProblem.cs
@@ -3,56 +3,82 @@ using System;
 
 namespace DataTypesAndVariables.Exercise1
 {
-   public class NumbersProblem
+    public class NumbersProblem
     {
-        
+
+
         public static void Main()
         {
-            // Create a program that will do the following and write all results to the console
+            //Create a program that will do the following and write all results to the console 
+            //Make sure to write an understandable message with each result so you can easily read it at the end
+            //EX If adding two numbers together, display to the console a message like 
+            //"Adding (the first number you chose) + (the second number) = (the result)"
 
-            //Add two ints 
-
-            //Subtract to ints
-
-            //multiply two ints 
-
-            //Divide two ints 
-
-            //Mod two ints.
-
-            //print the results of each
-
-            //print the minvalue of an int using the min value method
-
-            //print the maxvalue of an int using the max value method
-
-            //Add two doubles together
-
-            //Subtract to doubles 
-
-            //multiply two doubles 
-
-            //Divide two doubles 
-
-            //Mod two doulbes.
-
-            //print the results of each displaying two decimal places
-            //print the results of each diplaying 4 decimal places
-            //print the max value of a double
-            //print the min value of a double
-
-            //divide an int value by a double and capture the value and then print the result;
-            //todo: decimal manipulation 
-            // int and decimal/double result
-            // add max value of int to max value of int and print the result as int
-            // same as double and decimal
-
-
-            //BOOL and OBJect class problems will neede to be created.
-
-
+            IntegerAnswers();
+            DoubleAnswers();
+            DoubleMeetsIntegerAnswer();
+            DecimalMeetsIntegerAnswer();
+            BooleanOperatorsAnswer();
+            ObjectAnswers();
             Console.ReadLine();
         }
 
+        public static void IntegerAnswers()
+        {
+            //Add two ints 
+            //Subtract two ints
+            //Multiply two ints 
+            //Divide two ints 
+            //Mod two ints
+            //Print the minvalue of an int using the min value method
+            //Print the maxvalue of an int using the max value method
+            //Add max value of int to max value of int and print the result as int
+            //Make a variable with keyword type int and then make a string variable and call the type of the int variable you made
+        }
+
+        public static void DoubleAnswers()
+        {
+            //Add two doubles together
+            //Subtract two doubles 
+            //multiply two doubles 
+            //Divide two doubles 
+            //Mod two doubles
+            //Print the results of doublePrecision displaying two decimal places
+            //Print the results of doublePrecision diplaying 4 decimal places
+            //Print the max value of a double
+            //Print the min value of a double
+            //Add max value of double to max value of double and print the result as int
+            //Make a variable with keyword type int and then make a string variable and call the type of the int variable you made
+        }
+
+        public static void DoubleMeetsIntegerAnswer()
+        {
+            //Divide an int value by a double and capture the value and then print the result and then the results type;
+        }
+
+        public static void DecimalMeetsIntegerAnswer()
+        {
+            //Create an integer number and decimal number and divide the integer by the decimal
+            //Display the results and the results type
+        }
+
+        public static void BooleanOperatorsAnswer()
+        {
+            //What would be the result of true && false, true && true, false && true, and false && false
+            //What would be the result of true || true, true || false, false || true, false || false
+            //What would be the result of true ^ true, true ^ false, false ^ true, false ^ false
+            //Make a variable with keyword type bool and then make a string variable and call the type of the int variable you made
+        }
+
+        public static void ObjectAnswers()
+        {
+            //Create two objects
+            //Compare the two objects and see if they are the same
+            //Create another object
+            //Now create another object that is set to the value of the last object you created
+            //Compare the last 2 objects together, do you think they are the same?
+            //Call .ToString() on the last two objects you made
+            //Get the type of the instance of the last object you made, represented as a string
+        }
     }
 }
